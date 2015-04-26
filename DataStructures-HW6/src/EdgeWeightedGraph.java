ccce512d33db073aa9472fb02fbbb3c1e879a752
David West <drwest15@gmail.com>
2015-04-26 15:45:35 -0400

    adding constructor

---
+++ DataStructures-HW6/src/EdgeWeightedGraph.java
@@ -0,0 +1,15 @@
+
+public interface EdgeWeightedGraph {
+	
+	/**
+	 * Add an edge to the graph
+	 * NEED PARAMS
+	 */
+	public void addEdge();
+	
+	
+	public boolean isEdge();
+	public void removeEdge();
+	public int getEdge();
+	public int getNeighbors();
+}
